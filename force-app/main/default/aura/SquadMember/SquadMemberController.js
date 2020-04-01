81b95ea3876e79c75da472a3d4a2b02a4397a55c
Alex Abdulla <alexander.amadeus.abdulla@gmail.com>
2020-03-31 18:50:06 -0700

    Added Account Validation Toasts

--- force-app/main/default/aura/SquadMember/SquadMemberController.js
+++ force-app/main/default/aura/SquadMember/SquadMemberController.js
@@ -2,7 +2,6 @@
     doInit : function(component, event) {
         console.log('starting init');
         var accountId = component.get("v.selectedAccount");
-        console.log(accountId);
         var action = component.get("c.getSquadMembers");
         action.setParams({
             accountId : accountId
@@ -14,5 +13,32 @@
         });
         $A.enqueueAction(action);
         //console.log(component.get("v.selectedAccount"));
+    },
+    
+    validateAccounts : function(component, event) {
+        console.log("validating accounts: " + event.getSource().get("v.label"));
+        var contactId = event.getSource().get("v.label");
+        var action = component.get("c.getAccountValidation");
+        action.setParams({
+            contactId : contactId
+        });
+
+        action.setCallback(this, function(a){
+            console.log('callback reached!');
+            console.log(a.getReturnValue());
+            var toastEvent = $A.get("e.force:showToast");
+            toastEvent.setParams({
+                title : 'Accounts Found',
+                message: a.getReturnValue(),
+                messageTemplate: 'Record {0} created! See it {1}!',
+                duration:' 3000',
+                key: 'info_alt',
+                type: 'info',
+                mode: 'dismissible'
+            });
+            toastEvent.fire();
+            //component.set("v.squadMembers", a.getReturnValue());
+        });
+        $A.enqueueAction(action);
     },
 })
